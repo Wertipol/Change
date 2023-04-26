531b3aba3f94e1407324f93239b489bf5e6ac51e
Андрей <129662312+Wertipol@users.noreply.github.com>
2023-04-26 20:55:35 +0700

    update name

--- terminal.py
+++ terminal.py
@@ -1,16 +1,16 @@
-product_money = int(input('Сколько стоит сумма всех товаров: '))
-user_money = int(input('Сколько вы даете денег продавцу: '))
-money = [5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1]
-fin_dif = []
+productMoney = int(input('Сколько стоит сумма всех товаров: '))
+userMoney = int(input('Сколько вы даете денег продавцу: '))
+coins = [5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1]
+changeCoins = []
 
-difa = user_money - product_money
-dif = difa
+change = userMoney - productMoney
+value = change
 
-while dif > 0:
-    for i in range(len(money)):
-        while dif >= money[i]:
-            dif -= money[i]
-            fin_dif.append(money[i])
+while value > 0:
+    for i in range(len(coins)):
+        while value >= coins[i]:
+            value -= coins[i]
+            changeCoins.append(coins[i])
 
-print(f'Ваша сдача должна составлять {difa} \nПо начилным это:')
-print(*fin_dif, sep=', ')
+print(f'Ваша сдача должна составлять: {change} \nПо начилным это:')
+print(*changeCoins, sep=', ')
